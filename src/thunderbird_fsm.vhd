f0381bef3fb30651858b6969cca2dbaa12006a72
georgeyork1 <george.york@afacademy.af.edu>
2025-03-14 15:54:16 -0600

    start

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -86,13 +86,18 @@ library ieee;
   use ieee.numeric_std.all;
  
 entity thunderbird_fsm is 
---  port(
-	
---  );
+    port (
+    i_clk, i_reset  : in    std_logic;
+    i_left, i_right : in    std_logic;
+    o_lights_L      : out   std_logic_vector(2 downto 0);
+    o_lights_R      : out   std_logic_vector(2 downto 0)
+);
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
+	type state_type is (sOFF, sON, R1, R2, R3, L1, L2, L3);
+	signal state: state_type;
 -- CONSTANTS ------------------------------------------------------------------
   
 begin
@@ -102,7 +107,55 @@ begin
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
+    state_proces: process(i_clk)  
+    begin
+        if (rising_edge(i_clk)) then
+            if (i_reset = '1') then 
+                state <= sOFF;
+            else 
+                case state is
+                    when sOFF =>
+                        if ((i_left = '1') and (i_right = '1')) then state <= sOFF; end if;
+                        if ((i_left = '0') and (i_right = '1')) then state <= R1; end if;
+                        if ((i_left = '1') and (i_right = '1')) then state <= L1; end if;      
+                    when sON => 
+                        state <= sOFF; 
+                    when R1 =>
+                        state <= R2;
+                    when R2 =>
+                        state <= R3; 
+                    when R3 =>
+                        state <= sOFF; 
+                    when L1 =>
+                        state <= L2;
+                    when L2 =>
+                        state <= L3; 
+                    when L3 =>
+                        state <= sOFF;                             
+                end case;
+            end if;
+        end if;
+    end process;
     
+    	-------------------------------------------------------------------------------
+    --        CW        meaning
+    --        [1,0]    RAM address counter
+    --        [2]    enables write of RAM    
+    -------------------------------------------------------------------------------
+    output_process: process (state)
+    begin
+        case state is
+            when sOFF => o_lights_L <= "000"; o_lights_R <= "000" ;
+            when sON  => o_lights_L <= "111"; o_lights_R <= "111" ;
+            when   R1 => o_lights_L <= "000"; o_lights_R <= "100" ;
+            when   R2 => o_lights_L <= "000"; o_lights_R <= "110" ;
+            when   R3 => o_lights_L <= "000"; o_lights_R <= "111" ;
+            when   L1 => o_lights_L <= "001"; o_lights_R <= "000" ;
+            when   L2 => o_lights_L <= "011"; o_lights_R <= "000" ;
+            when   L3 => o_lights_L <= "111"; o_lights_R <= "000" ;
+            when others =>  o_lights_L <= "000"; o_lights_R <= "000" ;
+        end case;
+    end process;
 	-----------------------------------------------------					   
 				  
 end thunderbird_fsm_arch;
